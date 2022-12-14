316864882b8264268ecbfe428ad9dc2448d4ec35
Shubham <subham.k.ojha@gmail.com>
2022-12-14 17:54:16 +0530

    Custom printf() completed

---
+++ kernel.cpp
@@ -0,0 +1,22 @@
+// #include <cstdio>
+/*
+    cant include header files, as we are outside of the operating system
+    so writing our own printf
+
+    0xb8000 --> memory location that will display words written there
+*/
+
+
+void printf(char* str){
+    
+    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    for(int i=0;str[i]!='\0';i++){
+        VideoMemory[i]= (VideoMemory[i] & 0xFF00) | str[i];
+    }
+}
+
+
+void kernelMain(void* multiboot_structure, unsigned int magic_number){
+    printf("HELLO WORLD! \n\tThis is xOS!!");
+    while(1);
+}
